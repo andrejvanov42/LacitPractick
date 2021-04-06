cec33cfd02034a4c5937b24c310db14e1b1a9fcf
andrejvanov42 <andrejvanov42@gmail.com>
2021-04-06 10:23:51 +0300

    modified'

--- homework.py
+++ homework.py
@@ -1,27 +1,42 @@
+ #1
 a = [1, 2 , 6, 1.2, 'a']
-b = [x for x in a if type(x) == int and x%2 == 0 ] #1
+b = [x for x in a if type(x) == int and x%2 == 0 ]
 print(b)
+
+#2
 d = [-1, 2 , 6, 1.2]
-c = d[::2] #2
+c = d[::2] 
 print(c)
 
-e = [x for x in d if x>0] #3
+ #3
+d = [-1, 2 , 6, 1.2]
+e = [x for x in d if x>0]
 print(e)
 
+ #4
 m = 3
 l = [1, 3, 4, 7, 9]
-l1 = [x for x in l if x%m == 0] #4
+l1 = [x for x in l if x%m == 1]
 print(l1)
 
+
+#5
 dictionary = {'b':2, 3:4, 'a':9, 4:1}
-res = [x+y for x, y in dictionary.items() if type(x) in [int, float] and type(y) in [int, float] ]
+res = [x+y for x, y in dictionary.items() if type(x) in [int, float] and type(y) in [int, float] ] 
 print(res)
 
+#6
+L = [3, 2, 4, 4, -1]
+L.sort()
+L1 = [(L[i],L[j])for i in range(len(L)-1) for j in range(i+1, len(L))]
+print(L1)
+#баг если одинаковые 
+#7
 l = ['a', 1, -2, 1.2]
-
-non_even = [i for i in l if l.index(i)%2!=0]
-even = [i for i in l if l.index(i)%2==0]
-d = {
-    even[i]: non_even[i] for i in range(even) 
-}
+value = [i for i in l if l.index(i)%2!=0]
+keys = [i for i in l if l.index(i)%2==0]
+d = dict.fromkeys(keys, value)
 print(d)
+
+
+
